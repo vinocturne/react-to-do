09b7bb16fb8af9a529f1c130da18803a1aad2b0e
Ghunhee Shin <hago@rocketstaff.com>
2022-04-25 14:42:11 +0900

    init

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./theme";
+
+const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+);
+root.render(
+    <React.StrictMode>
+        <RecoilRoot>
+            <ThemeProvider theme={darkTheme}>
+                <App />
+            </ThemeProvider>
+        </RecoilRoot>
+    </React.StrictMode>
+);
---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+    bgColor: "#2f3640",
+    textColor: "#000000",
+    accentColor: "#4cd137",
+    listColor: "#ffffff",
+};
