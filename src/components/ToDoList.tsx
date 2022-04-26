7efc94be2b998c69a1b1ac416d6e1c2c9251a09e
Ghunhee Shin <hago@rocketstaff.com>
2022-04-26 12:29:43 +0900

    Feat(컴포넌트 분리) : "추가 컴포넌트 분리 및 category 변경 로직"
    기능을 컴포넌트 별로 분리하고 버튼을 클릭했을 시 category 변경이 가능하도록 수정하였습니다.

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,53 +1,21 @@
-import { useForm } from "react-hook-form";
-import { atom, useRecoilState } from "recoil";
-
-interface IForm {
-    toDo: string;
-}
-
-interface IToDo {
-    text: string;
-    id: number;
-    category: "TO_DO" | "DOING" | "DONE";
-}
-
-const toDoState = atom<IToDo[]>({
-    key: "toDo",
-    default: [],
-});
+import { useRecoilValue } from "recoil";
+import { toDoState } from "../atoms";
+import CreateToDo from "./CreateToDo";
+import ToDo from "./ToDo";
 
 function ToDoList() {
-    // const value = useRecoilValue(toDoState);
-    // const modFn = useSetRecoilState(toDoState);
-    //필요할 때는 위에 처럼 따로 설정이 가능하지만, useState처럼 두개 동시에 설정도 가능하다.
-    const [toDos, setToDos] = useRecoilState(toDoState);
-    const { register, handleSubmit, setValue } = useForm<IForm>();
-    const handleValid = ({ toDo }: IForm) => {
-        setValue("toDo", "");
-        //setToDos를 할 경우에, 함수를 넣게되면 return값이 새로운 toDos가 된다.
-        //아래는 이전 값들을 그대로 반환하는 것.
-        setToDos((oldToDos) => [
-            { text: toDo, id: Date.now(), category: "TO_DO" },
-            ...oldToDos,
-        ]);
-    };
+    const toDos = useRecoilValue(toDoState);
     console.log(toDos);
     return (
         <div>
             <h1 style={{ color: "white" }}>To Dos</h1>
             <hr />
-            <form onSubmit={handleSubmit(handleValid)}>
-                <input
-                    {...register("toDo", {
-                        required: "Please write a To Do",
-                    })}
-                    placeholder="Write a to do"
-                />
-                <button>Add</button>
-            </form>
+            <CreateToDo />
             <ul style={{ color: "white" }}>
                 {toDos.map((toDo) => (
-                    <li key={toDo.id}>{toDo.text}</li>
+                    // <ToDo text={toDo.text} category={toDo.category} id={toDo.id} />
+                    // 위처럼 모든 프로퍼티를 넘기는 것 보다 아래와 같이 ...을 활용하여 한번에 옮기는 것이 편하고 보기 좋다.
+                    <ToDo key={toDo.id} {...toDo} />
                 ))}
             </ul>
         </div>
