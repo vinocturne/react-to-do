13c1e575e72dbe31a0e0eb95f83b1ff9c48419bd
Ghunhee Shin <hago@rocketstaff.com>
2022-04-26 18:00:30 +0900

    Fix(로컬스토리지 사용) : "추가 로컬스토리지 사용 및 삭제 기능, 컴포넌트 스타일 변경"
    로컬 스토리지 사용 및 컴포넌트 스타일을 변경하였습니다.
    작성하였던 toDo를 삭제할 수 있도록 수정하였습니다.
    toastify를 통해 업데이트, 삭제, 이동 시 확인 가능하도록 수정하였고,
    ui 라이브러리인 mui material를 사용하여 진행하였습니다.

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,44 +1,97 @@
 import { Categories, IToDo, toDoState } from "../atoms";
 import { useSetRecoilState } from "recoil";
+import styled from "styled-components";
+import { IconButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import FormatListBulletedTwoToneIcon from "@mui/icons-material/FormatListBulletedTwoTone";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import HourglassTopTwoToneIcon from "@mui/icons-material/HourglassTopTwoTone";
+import { toast } from "react-toastify";
+
+const List = styled.li`
+    list-style: none;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: white;
+    padding: 10px 15px;
+    border-radius: 20px;
+    box-shadow: rgba(215, 218, 220, 0.444) 0px 3px 24px;
+`;
 
 function ToDo({ text, category, id }: IToDo) {
     const setToDos = useSetRecoilState(toDoState);
+
     const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         const {
             currentTarget: { name },
         } = event;
         setToDos((oldToDos) => {
+            const modifiedToDo = { text, id, category: name as any };
             const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
-            const newToDo = { text, id, category: name as any };
-            return [
+            const list = [
                 ...oldToDos.slice(0, targetIndex),
-                newToDo,
+                modifiedToDo,
                 ...oldToDos.slice(targetIndex + 1),
             ];
+            localStorage.setItem("toDoList", JSON.stringify(list));
+            toast.info(`Your To Do have been moved to "${name}" category`);
+            return list;
         });
     };
+
+    const onClickDelete = () => {
+        setToDos((oldToDos) => {
+            const list = [...oldToDos.filter((toDo) => toDo.id !== id)];
+            toast.error("Your To Do have been removed.");
+            localStorage.setItem("toDoList", JSON.stringify(list));
+            return list;
+        });
+    };
+
     return (
-        <li>
+        <List>
             <span>{text}</span>
-            {category !== Categories.DOING && (
-                // <button onClick={() => onClick("DOING")}>Doing</button>
-                <button name={Categories.DOING} onClick={onClick}>
-                    Doing
-                </button>
-            )}
-            {category !== Categories.TO_DO && (
-                // <button onClick={() => onClick("TO_DO")}>To Do</button>
-                <button name={Categories.TO_DO} onClick={onClick}>
-                    To Do
-                </button>
-            )}
-            {category !== Categories.DONE && (
-                // <button onClick={() => onClick("DONE")}>Done</button>
-                <button name={Categories.DONE} onClick={onClick}>
-                    Done
-                </button>
-            )}
-        </li>
+            <div>
+                {category !== Categories.DOING && (
+                    <IconButton
+                        aria-label="doing"
+                        color="info"
+                        name={Categories.DOING}
+                        onClick={onClick}
+                    >
+                        <HourglassTopTwoToneIcon />
+                    </IconButton>
+                )}
+                {category !== Categories.TO_DO && (
+                    <IconButton
+                        aria-label="toDo"
+                        color="secondary"
+                        name={Categories.TO_DO}
+                        onClick={onClick}
+                    >
+                        <FormatListBulletedTwoToneIcon />
+                    </IconButton>
+                )}
+                {category !== Categories.DONE && (
+                    <IconButton
+                        aria-label="done"
+                        color="success"
+                        name={Categories.DONE}
+                        onClick={onClick}
+                    >
+                        <CheckCircleOutlineIcon />
+                    </IconButton>
+                )}
+                <IconButton
+                    aria-label="delete"
+                    color="error"
+                    onClick={onClickDelete}
+                >
+                    <DeleteIcon />
+                </IconButton>
+            </div>
+        </List>
     );
 }
 
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,31 +1,90 @@
-import { useRecoilState, useRecoilValue } from "recoil";
-import { Categories, categoryState, toDoSelector } from "../atoms";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { Categories, categoryState, toDoSelector, toDoState } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
+import styled from "styled-components";
+import { BootstrapInput } from "../assets/component-styled";
+import { MenuItem, Select, SelectChangeEvent } from "@mui/material";
+import FormControl from "@mui/material/FormControl";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
+
+const Container = styled.div`
+    margin: 0 auto;
+    width: 400px;
+    padding: 30px 15px;
+`;
+const Title = styled.h1`
+    color: white;
+    font-size: 32px;
+`;
+const CreateContainer = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+const ListContainer = styled.div`
+    padding: 10px 0;
+    display: flex;
+    flex-direction: column;
+    gap: 13px;
+`;
 
 function ToDoList() {
     const toDos = useRecoilValue(toDoSelector);
+    const setToDos = useSetRecoilState(toDoState);
     const [category, setCategory] = useRecoilState(categoryState);
-    const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
-        //타입스크립트는 option의 value가 categories 타입이라는 것을 모르고
-        //string으로만 받아들이기 때문에 as any를 통해 모든 값이 들어갈 수 있도록 해주면 된다.
-        setCategory(event.currentTarget.value as any);
+    const onChange = (event: SelectChangeEvent<Categories>) => {
+        const {
+            target: { value },
+        } = event;
+
+        setCategory(value as any);
     };
-    console.log(category);
+    useEffect(() => {
+        const loadToDoList: string = localStorage.getItem("toDoList") as any;
+        if (loadToDoList !== null) {
+            setToDos(() => JSON.parse(loadToDoList));
+        }
+    }, []);
+
     return (
-        <div>
-            <h1 style={{ color: "white" }}>To Dos</h1>
+        <Container>
+            <Title>📑 To Dos</Title>
             <hr />
-            <select value={category} onInput={onInput}>
-                <option value={Categories.TO_DO}>To Do</option>
-                <option value={Categories.DOING}>Doing</option>
-                <option value={Categories.DONE}>Done</option>
-            </select>
-            <CreateToDo />
-            {toDos?.map((toDo) => (
-                <ToDo key={toDo.id} {...toDo} />
-            ))}
-        </div>
+            <CreateContainer>
+                <FormControl sx={{ m: 1 }} variant="standard">
+                    <Select
+                        value={category}
+                        label="state"
+                        input={<BootstrapInput />}
+                        onChange={onChange}
+                        size="small"
+                    >
+                        <MenuItem value={Categories.TO_DO}>To Do</MenuItem>
+                        <MenuItem value={Categories.DOING}>Doing</MenuItem>
+                        <MenuItem value={Categories.DONE}>Done</MenuItem>
+                    </Select>
+                </FormControl>
+                <CreateToDo />
+            </CreateContainer>
+            <ListContainer>
+                {toDos?.map((toDo) => (
+                    <ToDo key={toDo.id} {...toDo} />
+                ))}
+            </ListContainer>
+            <ToastContainer
+                position="bottom-center"
+                autoClose={2000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
+        </Container>
     );
 }
 
